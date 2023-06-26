ec763070d8df7ddab27af0775fd0014bd85f2640
JERE-GIM <arubu3667@gmail.com>
2023-06-27 00:58:04 +0900

    first commit

--- client/src/constants/Numbers.ts
+++
@@ -1,6 +0,0 @@
-export const Numbers = {
-  SVG_WIDTH: 26,
-  SVG_HEIGHT: 26,
-  SVG_FILL_COLOR: '#0b95ff',
-  OPACITY: 0.5,
-};
--- client/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postReducer from './post/postSlice';
-import loginReducer from './loginSlice';
-
-export const store = configureStore({
-  reducer: { post: postReducer, login: loginReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
