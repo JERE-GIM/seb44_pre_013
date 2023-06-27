ee4eabb387167bb0e178ee5560f11b72de09ef8c
JERE-GIM <arubu3667@gmail.com>
2023-06-27 11:22:12 +0900

    fix: QuestionMapper 수정

--- src/main/java/server/question/mapper/QuestionMapper.java
+++ src/main/java/server/question/mapper/QuestionMapper.java
@@ -3,10 +3,14 @@ package server.question.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import server.answer.dto.AnswerResponseDto;
+import server.answer.entity.Answer;
 import server.question.dto.QuestionDto;
 import server.question.entity.Question;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface QuestionMapper {
@@ -14,8 +18,49 @@ public interface QuestionMapper {
 
     Question questionPatchDtoToQuestion(QuestionDto.Patch requestBody);
 
-    @Mapping(source = "member.memberId", target = "memberId")
-    QuestionDto.Response questionToQuestionResponseDto(Question question);
+    default QuestionDto.Response QuestionToQnaQuestionResponseDto(Question question, List<Answer> answers) {
+        return
+                QuestionDto.Response.builder()
+                        .questionId(question.getQuestionId())
+                        .title(question.getTitle())
+                        .content(question.getContent())
+                        .memberId(question.getMember().getMemberId())
+                        .viewCount(question.getViewCount())
+                        .answers(answerListToAnswerResponseDtoList(question.getAnswers()))
+                        .createdAt(question.getCreatedAt())
+                        .modifiedAt(question.getModifiedAt())
+                        .build();
+    }
+
+    private List<AnswerResponseDto> answerListToAnswerResponseDtoList(List<Answer> list) {
+        if (list == null) {
+            return null;
+        }
+
+        List<AnswerResponseDto> list1 = new ArrayList<AnswerResponseDto>(list.size());
+        for (Answer answer : list) {
+            list1.add(answerToAnswerResponseDto(answer));
+        }
+
+        return list1;
+    }
+
+    private AnswerResponseDto answerToAnswerResponseDto(Answer answer) {
+        if ( answer == null ) {
+            return null;
+        }
+
+        AnswerResponseDto.AnswerResponseDtoBuilder answerResponseDto = AnswerResponseDto.builder();
+
+        answerResponseDto.answerId( answer.getAnswerId() );
+        answerResponseDto.questionId( answer.getQuestionId() );
+        answerResponseDto.memberId( answer.getMemberId() );
+        answerResponseDto.content( answer.getContent() );
+        answerResponseDto.createdAt( answer.getCreatedAt() );
+        answerResponseDto.modifiedAt( answer.getModifiedAt() );
+
+        return answerResponseDto.build();
+    }
 
     @Mapping(source = "answers", target = "answers", ignore = true)
     List<QuestionDto.Response> questionsToQuestionResponseDtos(List<Question> questions);
